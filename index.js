7091e056f7c9d67fdbc56bbe954619c3f04a267e
Bheemaraya <105291512+BheemuUppar@users.noreply.github.com>
2023-07-18 10:55:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ function authenticateToken(req, res, next) {
 }
 
 // Start the server
-const port = 3000;
+const port =  process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
